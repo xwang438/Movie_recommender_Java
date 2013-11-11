fe3b3f4b42cc5b56e53e8bea50fa4875d4a2ac0a
Xinlu Wang <xinlu0402@gmail.com>
2013-11-11 17:20:31 -0500

    update raw dataset

--- training_test.java
+++ training_test.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,47 +8,17 @@ import java.util.Random;
  * This class segments the ratings dataset into training data and test data
  * Randomly take 70% of the ratings as training data and 30% as test data
  *
- * There are 6040 users and 3952 movies
- * Use 100 movies to do experiment
+ * 100000 ratings by 943 users on 1682 items.Each user has rated at least 20 movies. 
+ * Users and items numbered consecutively from 1.
+ * 
+ * u.data: user id | item id | rating | timestamp. 
  */
 public class training_test {
 	/**
-	 * Put the movies, users, ratings and timestamp in each line into an ArrayList containing:
-	 * MovieID, userID, rating, Timestamp
-	 */
-	public static ArrayList<Integer[]> readInData(){
-		String filename = "ratings.dat";
-		BufferedReader br = null;
-		ArrayList<Integer[]> list = new ArrayList<Integer[]>();
-		try{
-			String reader = "";
-			br = new BufferedReader(new FileReader(filename));
-			while((reader = br.readLine()) != null){
-				int k = 0, j = 0, i = 0;
-				Integer[] rat = new Integer[4];
-				while(k < reader.length() && i < 4){
-					if(reader.charAt(k) == ':'){
-						rat[i++] = Integer.parseInt(reader.substring(j,  k));
-				        j = k + 2;
-				        k = j - 1;
-					}
-					k++;
-				}
-				rat[i] = Integer.parseInt(reader.substring(j));
-				list.add(rat);
-			}
-			br.close();
-		}
-		catch(IOException e){
-            e.printStackTrace();
-        }
-		return list;
-	}
-	/**
 	 * Get the training data and test data
 	 */
-	public static ArrayList<ArrayList<Integer[]>> train_test(){
-		ArrayList<Integer[]> list = readInData();
+	public static ArrayList<ArrayList<String[]>> train_test(){
+		ArrayList<String[]> list = read_file.readInData("u.data", '\t');
 		int size = list.size();
 		int test = (int)(size * 0.3);
 		int[] randselect = new int[size];
@@ -67,15 +35,15 @@ public class training_test {
 			    count--;
 		    }
 		}
-		ArrayList<Integer[]> testlist = new ArrayList<Integer[]>();
-		ArrayList<Integer[]> trainlist = new ArrayList<Integer[]>();
+		ArrayList<String[]> testlist = new ArrayList<String[]>();
+		ArrayList<String[]> trainlist = new ArrayList<String[]>();
 		for(int i = 0; i < list.size(); i++)
 		    trainlist.add(list.get(i));
 		for(int i = 0; i < testset.size(); i++){
 			testlist.add(list.get(i));
 			trainlist.remove(i);
 		}
-		ArrayList<ArrayList<Integer[]>> result = new ArrayList<ArrayList<Integer[]>>();
+		ArrayList<ArrayList<String[]>> result = new ArrayList<ArrayList<String[]>>();
 		result.add(trainlist);
 		result.add(testlist);
 		return result;
@@ -84,15 +52,15 @@ public class training_test {
 	 * Create the new files containing the training data and test data separately
 	 */
 	public static void createFile(){
-		ArrayList<ArrayList<Integer[]>> list = train_test();
-		ArrayList<Integer[]> train = list.get(0);
-		ArrayList<Integer[]> test = list.get(1);
+		ArrayList<ArrayList<String[]>> list = train_test();
+		ArrayList<String[]> train = list.get(0);
+		ArrayList<String[]> test = list.get(1);
 		BufferedWriter bw = null;
 		try{
             bw = new BufferedWriter(new FileWriter("training"));
             for(int i = 1; i < train.size(); i++){
-            	for(int s : train.get(i)){
-                     bw.write(String.valueOf(s));
+            	for(String s : train.get(i)){
+                     bw.write(s);
          	         bw.write("\t");
             	}
             	bw.write("\n");
@@ -100,8 +68,8 @@ public class training_test {
             bw.close();
             bw = new BufferedWriter(new FileWriter("test"));
             for(int i = 1; i < test.size(); i++){
-            	for(int s : test.get(i)){
-                     bw.write(String.valueOf(s));
+            	for(String s : test.get(i)){
+                     bw.write(s);
          	         bw.write("\t");
             	}
             	bw.write("\n");
@@ -113,8 +81,7 @@ public class training_test {
         }
 	}
 	public static void main(String[] args){
-		ArrayList<ArrayList<Integer[]>> set = train_test();
-		
 		createFile();
 	}
 }
+
